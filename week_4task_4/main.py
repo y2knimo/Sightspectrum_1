495a37265847b4bb36b5e59ec77712090b1ce895
unknown <y2knimo>
2023-06-22 02:09:59 -0700

    first commit

---
+++ week_4task_4/main.py
@@ -0,0 +1,15 @@
+
+def fun(name):
+    print("Maybe " + name)
+fun("more")
+fun("mani")
+fun("your self Gopi")
+
+def fun(*name):
+    print("Always kill by " + name[1])
+fun("Gopi" , "kishore")
+
+
+
+
+

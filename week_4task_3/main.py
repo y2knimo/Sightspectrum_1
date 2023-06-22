6b6ddc3f2c355abec6b9df3ac38126cb659d1c0a
unknown <y2knimo>
2023-06-22 02:06:43 -0700

    first commit

--- week_4task_3/main.py
+++ week_4task_3/main.py
@@ -9,12 +9,7 @@ def fun(*name):
     print("Always kill by " + name[1])
 fun("Gopi" , "kishore")
 
-def fun(name3,name2,name1):
-    print("The small age child is " + name3)
-fun(name1="Gopi",name2="Kishore",name3="Mani")
 
 
-def fun(**kid):
-    print("What have " + kid["last"])
-fun(first=" Gopi",last=" I Think so")
+
 
